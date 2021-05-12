5d9ef999381ac3fe714aeb0bdd9fc52bcca18725
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-05-12 20:10:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,48 +4,20 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var myEngine, myWorld;
-var bg;
-var ground;
-var boggie1,boggie2,boggie3;
-var chain1;
-var trainSound 
-var crashSound
+
 var flag = 0;
 
 function preload(){
-  bg= loadImage("images/bg.jpg");
-  trainSound = loadSound("sound/train.mp3");
-  crashSound = loadSound("sound/train_crossing.mp3");
+ 
 }
 function setup() {
   createCanvas(1200,400);
-  myEngine = Engine.create();
-  myWorld= myEngine.world;
-
-  ground = new Ground(600,380,1200,20);
-boggie1=new Boggie(50,170,50,50);
-
-
-
-rock1= new Rock(1100,200,100,100);
-chain1 = new Chain(boggie1.body,boggie2.body);
-
-
 
 }
 
 function draw() {
   background(bg);  
-  Engine.update(myEngine);
-
-  boggie1.show();
-  boggie2.show();
-  
-  rock1.show();
-  
-  chain1.show();
-
-
+ 
   }
 
   function keyPressed()
